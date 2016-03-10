5eb76e9f1884fa9344f309a3c294c3998aff2a4c
AgnesBLumatao <a.lumatao@klab.com>
2016-03-10 12:24:30 +0800

    String manipulation exercise.

---
+++ string2.py
@@ -0,0 +1,100 @@
+#!/usr/bin/python
+# Additional basic string exercises
+
+
+# D. verbing
+# Given a string, if its length is at least 3,
+# add 'ing' to its end.
+# Unless it already ends in 'ing', in which case
+# add 'ly' instead.
+# If the string length is less than 3, leave it unchanged.
+# Return the resulting string.
+def verbing(s):
+    if len(s) < 3:
+        return s
+    if len(s) >= 3 and 'ing' in s:
+        return s + 'ly'
+    else:
+        return s + 'ing'
+
+
+# E. not_bad
+# Given a string, find the first appearance of the
+# substring 'not' and 'bad'. If the 'bad' follows
+# the 'not', replace the whole 'not'...'bad' substring
+# with 'good'.
+# Return the resulting string.
+# So 'This dinner is not that bad!' yields:
+# This dinner is good!
+def not_bad(s):
+    not_index = s.find('not')
+    bad_index = s.find('bad')
+    if not_index < bad_index:
+        return s.replace(s[not_index:bad_index+len('bad')], 'good')
+    else:
+        return s
+
+
+# F. front_back
+# Consider dividing a string into two halves.
+# If the length is even, the front and back halves are the same length.
+# If the length is odd, we'll say that the extra char goes in the front half.
+# e.g. 'abcde', the front half is 'abc', the back half 'de'.
+# Given 2 strings, a and b, return a string of the form
+#  a-front + b-front + a-back + b-back
+def front_back(a, b):
+    front_a = ''
+    front_b = ''
+    a_len = len(a)
+    b_len = len(b)
+    a_front_len = len(a) / 2
+    b_front_len = len(b) / 2
+    if a_len % 2 == 0:
+        front_a = a[0:a_front_len]
+        back_a = a[a_front_len:]
+    else:
+        front_a = a[0:a_front_len+1]
+        back_a = a[a_front_len+1:]
+
+    if b_len % 2 == 0:
+        front_b = b[0:b_front_len]
+        back_b = b[b_front_len:]
+    else:
+        front_b = b[0:b_front_len+1]
+        back_b = b[b_front_len+1:]
+
+    new_string = front_a + front_b + back_a + back_b
+    return new_string
+
+
+# Simple provided test() function used in main() to print
+# what each function returns vs. what it's supposed to return.
+def test(got, expected):
+    if got == expected:
+        prefix = ' OK '
+    else:
+        prefix = '  X '
+    print('{} got: {} expected: {}'.format(prefix, repr(got), repr(expected)))
+
+
+# main() calls the above functions with interesting inputs,
+# using the above test() to check if the result is correct or not.
+def main():
+    print('verbing')
+    test(verbing('hail'), 'hailing')
+    test(verbing('swiming'), 'swimingly')
+    test(verbing('do'), 'do')
+
+    print('\nnot_bad')
+    test(not_bad('This movie is not so bad'), 'This movie is good')
+    test(not_bad('This dinner is not that bad!'), 'This dinner is good!')
+    test(not_bad('This tea is not hot'), 'This tea is not hot')
+    test(not_bad("It's bad yet not"), "It's bad yet not")
+
+    print('\nfront_back')
+    test(front_back('abcd', 'xy'), 'abxcdy')
+    test(front_back('abcde', 'xyz'), 'abcxydez')
+    test(front_back('Kitten', 'Donut'), 'KitDontenut')
+
+if __name__ == '__main__':
+    main()
